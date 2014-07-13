02ca698d05747b300a847c4cf85e03d3f04a6fa2
kapilsharma <kapil@kapilsharma.info>
2014-07-13 12:43:30 +0530

    Updated register page. Not perfect but got initial structure

--- modules/KapsBudget/register.php
+++ modules/KapsBudget/register.php
@@ -13,36 +13,7 @@
 
                 <!-- Main content -->
                 <section class="content">
-
-                    <!-- Small boxes (Stat box) -->
-                    <?PHP include $config['base_path'] . "parts/widgets/quickview.php"; ?>
-
-                    <!-- top row -->
-                    <div class="row">
-                        <div class="col-xs-12 connectedSortable">
-                            ss
-                        </div><!-- /.col -->
-                    </div>
-                    <!-- /.row -->
-
-                    <!-- Main row -->
-                    <div class="row">
-                        <!-- Left col -->
-                        <section class="col-lg-6 connectedSortable">
-                            <?PHP include $config['base_path'] . "parts/widgets/server-load.php"; ?>
-                            <?PHP include $config['base_path'] . "parts/widgets/sales.php"; ?>
-                            <?PHP include $config['base_path'] . "parts/widgets/calendar.php"; ?>
-                            <?PHP include $config['base_path'] . "parts/widgets/quick-mail.php"; ?>
-                        </section><!-- /.Left col -->
-
-                        <!-- right col (We are only adding the ID to make the widgets sortable)-->
-                        <section class="col-lg-6 connectedSortable">
-                            <?PHP include $config['base_path'] . "parts/widgets/mapbox.php"; ?>
-                            <?PHP include $config['base_path'] . "parts/widgets/chatbox.php"; ?>
-                            <?PHP include $config['base_path'] . "parts/widgets/todo.php"; ?>
-
-                        </section><!-- right col -->
-                    </div><!-- /.row (main row) -->
+                    <?PHP include $config['base_path'] . "parts/pages/register_contents.php"; ?>                    
                 </section><!-- /.content -->
             </aside><!-- /.right-side -->
         </div><!-- ./wrapper -->
---
+++ parts/pages/register_contents.php
@@ -0,0 +1,51 @@
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="box">
+                                <div class="box-header">
+                                    <h3 class="box-title">Data Table With Full Features</h3>                                    
+                                </div><!-- /.box-header -->
+                                <div class="box-body table-responsive">
+                                    <table id="example1" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Date</th>
+                                                <th>Description</th>
+                                                <th>Amount</th>
+                                                <th>Category</th>
+                                                <th>Account</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>12-Jan-2014</td>
+                                                <td>Milk 1 Lt</td>
+                                                <td>50</td>
+                                                <td>Home: Daily Expanses</td>
+                                                <td>Ritu Waller</td>
+                                                <td>edit, delete</td>
+                                            </tr>
+                                            <tr>
+                                                <td>12-Jan-2014</td>
+                                                <td>Vegitables</td>
+                                                <td>35</td>
+                                                <td>Home: Daily Expanses</td>
+                                                <td>Ritu Waller</td>
+                                                <td>edit, delete</td>
+                                            </tr>
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th>Date</th>
+                                                <th>Description</th>
+                                                <th>Amount</th>
+                                                <th>Category</th>
+                                                <th>Account</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                </div><!-- /.box-body -->
+                            </div><!-- /.box -->
+                        </div>
+                    </div>
